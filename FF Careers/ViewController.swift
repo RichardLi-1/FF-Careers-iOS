bfab57f6d59517f78b9a48e258b0402d619370a5
Richard Li <79170351+RichardLi-1@users.noreply.github.com>
2024-12-09 21:10:24 -0500

    Initial Commit

---
+++ FF Careers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FF Careers
+//
+//  Created by Richard Li on 2024-12-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
